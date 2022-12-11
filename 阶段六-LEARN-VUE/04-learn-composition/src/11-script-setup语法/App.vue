45a8657a35464a6e2c2e6889a218b13a412f6b55
ZXK <zhangxiaokang5389@163.com>
2022-12-11 23:19:23 +0800

    补充

---
+++ 阶段六-LEARN-VUE/04-learn-composition/src/11-script-setup语法/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+import ShowInfo from './ShowInfo.vue';
+// setup函数中的顶层模板会暴露给template中
+const message = '你好!JavaScript'
+let num = ref(0)
+const increment = () => {
+  num.value++
+}
+</script>
+
+<template>
+  <div>
+    <ShowInfo></ShowInfo>
+    <h2>{{ message }}</h2>
+    <h2>{{ num }}</h2>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+<style scoped>
+
+</style>
---
+++ 阶段六-LEARN-VUE/04-learn-composition/src/11-script-setup语法/ShowInfo.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h2>王宏源,19,1.88</h2>
+  </div>
+</template>
+<style scoped>
+
+</style>
